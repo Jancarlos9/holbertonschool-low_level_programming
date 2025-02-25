4e3a5c3d740ef166aabdd47ade29faf016f93be4
Jan Alicea <git@github.com:Jancarlos9/holbertonschool-shell.git>
2025-02-25 13:10:13 -0400

    char a<z l,case

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main alphabet in lowercase
+ Return Always (0)
+ */
+int main(void)
+{
+	char letter a;
+
+	if (a < z)
+	{
+		putchar (letter);
+		letter++;
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
+
+
